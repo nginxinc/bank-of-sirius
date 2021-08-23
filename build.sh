15cbeda3832c9e9158861baea20364c433104665
Elijah Zupancic <e.zupancic@f5.com>
2021-08-23 12:19:54 -0700

    Add Java base images

--- build.sh
+++ build.sh
@@ -5,19 +5,28 @@ set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-image_prefix="ghcr.io/bank-of-sirius"
+image_prefix="ghcr.io/nginxinc"
 docker_build_opts="--no-cache"
 
-cd "${script_dir}/base-images/python3"
-echo "Building image: ${image_prefix}/python3"
-docker build ${docker_build_opts} -t "${image_prefix}/python3" .
+if command -v mvn > /dev/null; then
+  echo "Using system Maven [$(command -v mvn)] to build java projects"
+  mvn -v
+  mvn_build_cmd="mvn"
+else
+  echo "Using Maven from Docker image to build java projects"
+  mvn_build_cmd="docker run -it --rm --name mvn_temp -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.3-jdk-8 mvn"
+fi
 
-cd "${script_dir}"
+maven_dirs_to_build="$(find "${script_dir}/src/" -maxdepth 2 -type f -name pom.xml -printf '%h\n')"
+for dir in $maven_dirs_to_build; do
+  cd "${dir}"
+  $mvn_build_cmd clean package
+done
 
-dirs_to_build="$(find "${script_dir}/src/" -maxdepth 2 -type f -name Dockerfile -printf '%h\n')"
+dirs_to_build="$(find "${script_dir}/base-images/" "${script_dir}/src/" -maxdepth 2 -type f -name Dockerfile -printf '%h\n')"
 for dir in $dirs_to_build; do
   dir_basename="$(basename "${dir}")"
-  image_name="${image_prefix}/${dir_basename}"
+  image_name="${image_prefix}/bos-${dir_basename}"
   cd "${dir}"
   echo "Building image: ${image_name}"
   docker build ${docker_build_opts} -t "${image_name}" .
