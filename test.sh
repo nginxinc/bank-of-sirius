8c20a0a1b61856896f42e3016af341e0523ced2c
Elijah Zupancic <e.zupancic@f5.com>
2021-08-23 19:35:00 -0700

    build: add build, test, publish prototype

--- test.sh
+++ test.sh
@@ -5,8 +5,19 @@ set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-image_prefix="ghcr.io/nginxinc"
-docker_build_opts="--no-cache"
+
+python_test_dirs="$(find "${script_dir}/src/" -maxdepth 2 -type f -name __init__.py -printf '%h\n')"
+python_test_dirs="$(find $python_test_dirs -maxdepth 1 -type d -name tests -printf '%h\n')"
+
+for dir in $python_test_dirs; do
+  if [ -d "${dir}/tests" ]; then
+    dir_basename="$(basename "${dir}")"
+    echo "[${dir_basename}] running Python unit test"
+    docker run -it --rm -v "${dir}/tests:/app/tests" "bos-${dir_basename}" bash -c "ln -s /app /${dir_basename} && python -m pytest -v --rootdir=/${dir_basename} -p no:warnings"
+  else
+    echo "No directory found at ${dir}/tests - skipping tests"
+  fi
+done
 
 if "${script_dir}/mvnw" -v > /dev/null; then
   echo "Using Maven wrapper to build java projects"
@@ -21,18 +32,5 @@ ${mvn_build_cmd} -v
 maven_dirs_to_build="$(find "${script_dir}/src/" -maxdepth 2 -type f -name pom.xml -printf '%h\n')"
 for dir in $maven_dirs_to_build; do
   cd "${dir}"
-  $mvn_build_cmd clean package
+  $mvn_build_cmd test
 done
-
-dirs_to_build="$(find "${script_dir}/base-images/" "${script_dir}/src/" -maxdepth 2 -type f -name Dockerfile -printf '%h\n')"
-for dir in $dirs_to_build; do
-  dir_basename="$(basename "${dir}")"
-  short_name="bos-${dir_basename}"
-  image_name="${image_prefix}/${dir_basename}"
-  cd "${dir}"
-  echo "Building image: ${image_name}"
-  docker build ${docker_build_opts} -t "${short_name}" .
-  docker tag "${short_name}" "${image_name}"
-done
-
-cd "${script_dir}"
