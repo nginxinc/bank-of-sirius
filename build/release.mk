ff707aa4ae7dea6c71270aea9c6a9899f31a4950
Elijah Zupancic <e.zupancic@f5.com>
2021-09-02 12:02:58 -0700

    refactor: rename python3 base image to python
    Signed-off-by: Elijah Zupancic <e.zupancic@f5.com>

--- build/release.mk
+++ build/release.mk
@@ -7,7 +7,7 @@ update-manifests: ## Updates Kubernetes manifest files to use the current deploy
 	$Q $(info $(M) changing kubernetes-manifests/*.yaml to use image v$(VERSION))
 	files="$$($(FIND) kubernetes-manifests/ -maxdepth 1 -mindepth 1 -type f -iname '*.yml' -or -iname '*.yaml' | xargs)"
 	$(DOCKER) run --tty --rm -v "$(CURDIR):/project" --workdir "/project" \
-		$(IMAGE_NAME_PREFIX)python3-dev \
+		$(IMAGE_NAME_PREFIX)python-dev \
 		python3 build/bin/update_manifest_image_versions.py 'v$(VERSION)' $${files}
 
 .PHONY: version
