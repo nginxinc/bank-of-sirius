44c28838b9a3a80e2d5ab043795ddd68f2b28aed
Elijah Zupancic <e.zupancic@f5.com>
2021-11-02 10:49:15 -0700

    feat: add additional tracing details to info endpoint
    Signed-off-by: Elijah Zupancic <e.zupancic@f5.com>

--- src/java-common/telemetry-common/src/main/java/sirius/samples/bankofsirius/endpoints/TracingAttributesInfoContributor.java
+++ src/java-common/telemetry-common/src/main/java/sirius/samples/bankofsirius/endpoints/TracingAttributesInfoContributor.java
@@ -1,23 +1,30 @@
 package sirius.samples.bankofsirius.endpoints;
 
 import com.google.common.collect.ImmutableMap;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.sdk.resources.Resource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
-import sirius.samples.bankofsirius.tracing.TracingAttributes;
+
+import java.util.function.Supplier;
 
 @Component
 public class TracingAttributesInfoContributor implements InfoContributor {
-    private final TracingAttributes tracingAttributes;
+    private final Supplier<Resource> tracingResourceSupplier;
 
-    public TracingAttributesInfoContributor(final TracingAttributes tracingAttributes) {
-        this.tracingAttributes = tracingAttributes;
+    public TracingAttributesInfoContributor(@Qualifier("applicationDetails")
+                                            final Supplier<Resource> tracingResourceSupplier) {
+        this.tracingResourceSupplier = tracingResourceSupplier;
     }
 
     @Override
     public void contribute(final Info.Builder builder) {
         final ImmutableMap.Builder<String, Object> mapBuilder = ImmutableMap.builder();
-        tracingAttributes.forEach((k, o) -> mapBuilder.put(k.getKey(), o));
+        final Resource resource = tracingResourceSupplier.get();
+        final Attributes attributes = resource.getAttributes();
+        attributes.forEach((k, o) -> mapBuilder.put(k.getKey(), o));
 
         builder.withDetail("trace.attributes", mapBuilder.build());
     }
