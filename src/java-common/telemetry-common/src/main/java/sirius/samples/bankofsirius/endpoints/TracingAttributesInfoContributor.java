bf0db7ada806d6fb04db634c890172f9952c4f43
Elijah Zupancic <e.zupancic@f5.com>
2021-11-17 08:17:27 -0800

    refactor: remove dependency on Google Guava
    This class was the only class in the module that used Google Guava.
    As such it was pulling in an unneeded dependency. This change
    simplifies our dependency graph by no longer using Guava.
    
    Signed-off-by: Elijah Zupancic <e.zupancic@f5.com>

--- src/java-common/telemetry-common/src/main/java/sirius/samples/bankofsirius/endpoints/TracingAttributesInfoContributor.java
+++ src/java-common/telemetry-common/src/main/java/sirius/samples/bankofsirius/endpoints/TracingAttributesInfoContributor.java
@@ -1,6 +1,5 @@
 package sirius.samples.bankofsirius.endpoints;
 
-import com.google.common.collect.ImmutableMap;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.sdk.resources.Resource;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,6 +7,8 @@ import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.function.Supplier;
 
 @Component
@@ -21,11 +22,11 @@ public class TracingAttributesInfoContributor implements InfoContributor {
 
     @Override
     public void contribute(final Info.Builder builder) {
-        final ImmutableMap.Builder<String, Object> mapBuilder = ImmutableMap.builder();
         final Resource resource = tracingResourceSupplier.get();
         final Attributes attributes = resource.getAttributes();
-        attributes.forEach((k, o) -> mapBuilder.put(k.getKey(), o));
+        final Map<String, Object> attrMap = new TreeMap<>();
+        attributes.forEach((k, o) -> attrMap.put(k.getKey(), o));
 
-        builder.withDetail("trace.attributes", mapBuilder.build());
+        builder.withDetail("trace.attributes", attrMap);
     }
 }
